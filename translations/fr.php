dd88e68a07e27b53ff4c2a355fc0d4693a46d35e
Michel Dumont <michel@dumont.io>
2021-06-27 19:26:49 +0200

    MISC : translations

--- translations/fr.php
+++ translations/fr.php
@@ -7,5 +7,6 @@ $_MODULE['<{mdgcategoryimage}prestashop>mdgcategoryimage_c581b86d72acfb813037231
 $_MODULE['<{mdgcategoryimage}prestashop>configurationform_96d4dadd096605d5813d2c6bdf82e82c'] = 'Paramètres enregistrés';
 $_MODULE['<{mdgcategoryimage}prestashop>configurationform_ef970dc2d200bfd1713b8e72d66cc6c8'] = 'Une erreur s\'est produite durant le processus';
 $_MODULE['<{mdgcategoryimage}prestashop>configurationform_9daf6056aa0d34486551a12e1d6b9163'] = 'Paramètres de la catégorie';
-$_MODULE['<{mdgcategoryimage}prestashop>configurationform_645a7248002d8eafe3ca1666c2c1df0f'] = 'Nombre d\'images supplémentaires';
+$_MODULE['<{mdgcategoryimage}prestashop>configurationform_5f2c0de83a592d6e5553fb59a874ecca'] = 'Nom des images supplémentaires';
+$_MODULE['<{mdgcategoryimage}prestashop>configurationform_bd87ac46cc9ed141a109f09854771228'] = 'Indiquez une image par ligne';
 $_MODULE['<{mdgcategoryimage}prestashop>configurationform_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
